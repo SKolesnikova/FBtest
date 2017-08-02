6e11aac2b607e3570fde44c367f4aeb37ff710a0
Svetlana.Kolesnikova <svetlana.kolesnikova@duniceedge.com>
2017-08-02 16:55:49 +0300

    Removed name form users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name
       user.image_uri = auth.info.image
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
